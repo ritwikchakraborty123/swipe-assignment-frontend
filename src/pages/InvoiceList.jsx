a01b3c737c89d0d968f091737dbc31dddff4d140
Ritwik Chakraborty <ritwikchakraborty.ritwik@gmail.com>
2024-07-24 17:37:42 +0530

    code refactoring

--- src/pages/InvoiceList.jsx
+++ src/pages/InvoiceList.jsx
@@ -1,12 +1,11 @@
 import React, { useState } from "react";
 import { Button, Card, Col, Row, Table } from "react-bootstrap";
-import { Link } from "react-router-dom";
 import { BiSolidPencil, BiTrash } from "react-icons/bi";
 import { BsEyeFill } from "react-icons/bs";
-import InvoiceModal from "../components/InvoiceModal";
-import { useNavigate } from "react-router-dom";
-import { useInvoiceListData } from "../redux/hooks";
 import { useDispatch } from "react-redux";
+import { Link, useNavigate } from "react-router-dom";
+import InvoiceModal from "../components/InvoiceModal";
+import { useInvoiceListData } from "../redux/hooks";
 import { deleteInvoice } from "../redux/invoicesSlice";
 
 const InvoiceList = () => {
